7e43140b1b6753b37b3bc367bc104d0bea1c5514
JJ Chen <jiajiechen95@gmail.com>
2023-06-28 20:36:14 -0400

    Initialize Astro project

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://jiajie-chen.github.io',
+	integrations: [mdx(), sitemap()],
+});
