949c922cb647901648c90fc6c1bf7e3a7249a46a
JJ Chen <jiajiechen95@gmail.com>
2023-07-06 21:53:12 -0400

    Initialize Astro & Tailwind integration

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
